f70bc96f6e2ee0c038cfedbdc7611ff64b04c650
Kerry Smith <kerrysfs@gmail.com>
2020-07-07 22:52:06 -0700

    trying to get heroku to work

--- src/App.js
+++ src/App.js
@@ -1,58 +1,44 @@
 import React, { Component } from 'react';
 
-import { CardList } from './components/card-list/card-list-component';
-import {Searchbox, searchBox} from './components/Searchbox/searchbox'
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
-constructor(){
-  super();
+  constructor() {
+    super();
 
-  this.state = {
-    monsters: [],
-    searchField: ''
-
-  };
-}
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
 
   componentDidMount() {
-  fetch('https://jsonplaceholder.typicode.com/users')
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(users => this.setState(({ monsters: users })));
+      .then(users => this.setState({ monsters: users }));
   }
+
+  onSearchChange = event => {
+    this.setState({ searchField: event.target.value });
+  };
 
   render() {
-  const { monsters, searchField } = this.state;
-  const filteredMonsters = monsters.filter(monster =>
-monster.name.toLowerCase().includes(searchField.toLowerCase())
-  )
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
-      <div className="App">
-       {/*<input*/}
-       {/*  type='search'*/}
-
-       {/*  onChange={e => {*/}
-       {/*    this.setState({searchField: e.target.value }, () => console.log(this.state)*/}
-       {/*    )*/}
-       {/*  }}*/}
-       {/*  />*/}
-         <Searchbox
-           placeholder='search monsters'
-           handleChange={e => {
-             this.setState({searchField: e.target.value }, () => console.log(this.state)
-             )}}
-         />
-
-
-        <CardList monsters={filteredMonsters}>
-
-        </CardList>
-
+      <div className='App'>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox onSearchChange={this.onSearchChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
-  );
+    );
   }
-}  
-
-
+}
 
 export default App;
