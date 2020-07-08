3d513a044fc7b20fbbad905debc4c5e0eefee1a9
Kerry Smith <kerrysfs@gmail.com>
2020-07-07 23:20:59 -0700

    ran prod build local to try and get it up and running

---
+++ README.md
@@ -0,0 +1,19 @@
+# monsters-rolodex
+
+The goal of this project was to learn some basics of React
+I created this website by implementing:
+
+* NPM
+* The create-react-app command
+* Class Components
+* JSX
+* Fetching content from a JSON placeholder
+* React Events
+* Filtering states
+* Arrow Functions
+
+Date: March 2020
+
+## Demo
+Demo accessible on https://wongdarren.github.io/monsters-rolodex/
+
---
+++ precache-manifest.966364088a6c8d66633b40324f23924c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde26aa7132dee3f1a8970f36fc51ac6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "18447d3dcd59e4530fd1",
+    "url": "/monsters-rolodex/static/css/main.d2d62635.chunk.css"
+  },
+  {
+    "revision": "c7c060b7febb674bc7a3",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.78892bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18447d3dcd59e4530fd1",
+    "url": "/monsters-rolodex/static/js/main.436303b8.chunk.js"
+  },
+  {
+    "revision": "24034436bd08b53a5646",
+    "url": "/monsters-rolodex/static/js/runtime-main.6386504b.js"
+  }
+]);
